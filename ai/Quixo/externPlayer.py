6581052ddc7edd0b87312f8058e120ec990e5590
Guigxs <guigxs19@gmail.com>
2019-05-05 13:14:00 +0200

    Add external player

---
+++ ai/Quixo/externPlayer.py
@@ -0,0 +1,66 @@
+import cherrypy
+import sys
+import random
+import numpy as np
+
+class Server:
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.json_out()
+    def move(self):
+        # Deal with CORS
+        cherrypy.response.headers['Access-Control-Allow-Origin'] = '*'
+        cherrypy.response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+        cherrypy.response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, X-Requested-With'
+        if cherrypy.request.method == "OPTIONS":
+            return ''
+
+        body = cherrypy.request.json
+
+        game = body["game"]
+        print("")
+        print("######################################")
+        print("############# Stupid bot #############")
+        print("######################################")
+        print("")
+        print("--------------------------------------")
+        print("State of the game :")
+        print(np.resize(game, (5, 5)))
+        
+        first = len(body['moves'])
+
+        print("")
+        print("##########", first, "move(s) #########")   
+        print("--------------------------------------")
+        print("")
+        print("--Logs:--")
+
+
+        if first%2 == 0: #Premier joueur
+            power = 0
+            print("First player with: X ({}) !".format(power))
+            cube = input("Cube : ")
+            direction = input("Direction :")
+            print("-----------------------------------")
+            print("Send : X in", cube, "from", direction)
+            print("-----------------------------------")
+            return {'move' :{'cube' : cube, 'direction': direction}}
+
+        elif first%2 == 1: #Second joueur
+            power = 1
+            print("Second player with: O ({}) !".format(power))
+            cube = input("Cube : ")
+            direction = input("Direction :")
+            print("-----------------------------------")
+            print("Send : O in", cube, "from", direction)
+            print("-----------------------------------")
+            return {'move' :{'cube' : cube, 'direction': direction}}
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        port=int(sys.argv[1])
+    else:
+        port=8080
+
+    cherrypy.config.update({'server.socket_port': port})
+    cherrypy.quickstart(Server())
